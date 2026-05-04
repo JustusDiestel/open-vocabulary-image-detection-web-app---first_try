74ebda5aced9328a2f1ff6eee5ac26b1303916c0
justusdiestel <justusdiestel@web.de>
2026-05-04 12:34:30 +0800

    backend und frontend läuft

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,28 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
+
+@app.post("/detect")
+async def detect(file: UploadFile = File(...)):
+    return{
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "message:": "file successfully uploaded"
+    }
+
+
+
